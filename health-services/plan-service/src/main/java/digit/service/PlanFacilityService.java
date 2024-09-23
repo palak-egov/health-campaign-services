5ec3f8baf48a464c886b0e9421b322d23a487c25
Saloni-eGov <saloni.bajaj@egovernments.org>
2024-09-23 14:37:05 +0530

    Save local changes before pulling

---
+++ health-services/plan-service/src/main/java/digit/service/PlanFacilityService.java
@@ -0,0 +1,27 @@
+package digit.service;
+
+import digit.service.validator.PlanFacilityValidator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlanFacilityService
+{
+    private PlanFacilityValidator planFacilityValidator;
+
+    public PlanFacilityService(PlanFacilityValidator planFacilityValidator)
+    {
+        this.planFacilityValidator = planFacilityValidator;
+    }
+
+    /**
+     * This method processes the requests that come for updating plans facility
+     * @param body
+     * @return The response containing the updated plan facility.
+     */
+    public PlanFacilityResponse updatePlanFacility(PlanFacilityRequest body)
+    {
+        planFacilityValidator.validatePlanFacilityUpdate(body);
+        return null;
+    }
+}
+
---
+++ health-services/plan-service/src/main/java/digit/service/validator/PlanFacilityValidator.java
@@ -0,0 +1,9 @@
+package digit.service.validator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlanFacilityValidator
+{
+    private PlanFacility;
+}
